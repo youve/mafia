b757c11c1b571ffb3ab34fdab02ed60d40c444aa
Youve <minny.molly@gmx.com>
2018-11-26 09:51:53 +0100

    Use regex to catch whether I forgot to put the year in instead of try/catch. Added documentation.

--- mafiaDeadlines.py
+++ mafiaDeadlines.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 # mafiaDeadlines.py
+# usage:
+# ./mafiaDeadlines.py "2018 09:30:00" 10 >> ../schedule/files/main
 
 import argparse
 import datetime
+import re
 
 parser = argparse.ArgumentParser(description='Deadline sizes')
 parser.add_argument('start', metavar='start', type=str, default=datetime.datetime.now(),
@@ -13,13 +16,15 @@ parser.add_argument('emoji', metavar='emoji', type=str, help="emoji", nargs='?',
 
 args = parser.parse_args()
 
-try: 
+yearRegex = re.compile("^\d{4}")
+if yearRegex.search(args.start): # I remembered to put the year in
     start = datetime.datetime.strptime(args.start, '%Y %j %H:%M')
-except:
+else: # I forgot to put the year in
     start =  datetime.datetime.strptime(str(datetime.datetime.today().year) + ' ' + args.start, '%Y %j %H:%M')
 
 delta = datetime.timedelta(days=(args.length-1)/20)
 emoji = args.emoji
 
+#print the times to update the deadline size in a format `main` can understand
 for i in range(1,21):
     print((start + delta*i).strftime('%j %H:%M'), emoji, 'dl', 100+5*i)
